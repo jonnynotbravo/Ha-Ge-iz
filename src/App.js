5576b208d607f3a79cdee44d69af7082d2990196
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-04-21 20:19:13 -0400

    configured a generic not found page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Home from "./Home";
-import { Routes, Route } from "react-router-dom";
+import GenericNotFound from "./GenericNotFound";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/404" element={<GenericNotFound />} />
+        <Route path="*" element={<Navigate to="404" />} />
       </Routes>
     </div>
   );
---
+++ src/GenericNotFound.js
@@ -0,0 +1,8 @@
+const GenericNotFound = () => {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+    </div>
+  );
+};
+export default GenericNotFound;
