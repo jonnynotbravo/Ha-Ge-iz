6e5b494e89110b30014616a219f9695005f14289
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-04-21 22:16:47 -0400

    Started Home page draft

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,22 @@
 const Home = () => {
-  return <div>Home1</div>;
+  return (
+    <div>
+      <header>
+        <div>
+          <li>Schools</li>
+          <li>Contact</li>
+        </div>
+      </header>
+      <div id="schools">
+        <div className="school">School One</div>
+        <div className="school">School Two</div>
+        <div className="school">School Three</div>
+        <div className="school">School Four</div>
+        <div className="school">School Five</div>
+        <div className="school">School Six</div>
+        <div className="school">School Seven</div>
+      </div>
+    </div>
+  );
 };
 export default Home;
