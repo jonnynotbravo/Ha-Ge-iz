8cb9be2353c70bdf0cd0b5f486a34cda3f0b2a83
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-05-04 00:45:58 -0400

    Finished Header Components skeleton

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,15 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
-      <li>
-        <NavLink to="/schools">schools</NavLink>
-      </li>
-      <li>Contact</li>
+    <header id="header">
+      <nav className="links" style={{ "--items": 5 }}>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Schools</a>
+        <a href="#">Contact</a>
+        <a href="#">Admin</a>
+        <span className="line"></span>
+      </nav>
     </header>
   );
 };
