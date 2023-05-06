2148ed1af73eabdfb4f0dcb811ed2bfb17811c71
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-05-06 17:52:38 -0400

    About page finished

---
+++ src/About.js
@@ -0,0 +1,42 @@
+import Header from "./Header";
+
+const About = () => {
+  return (
+    <div id="about">
+      <Header />
+      <h1>About</h1>
+      <p>
+        Welcome to our website! Our platform is specifically designed to assist
+        students and parents in Ethiopia find and secure a place in partnered
+        schools. We understand how daunting and time-consuming the school
+        selection and registration process can be, which is why we have created
+        a simple and user-friendly platform to simplify the entire process. With
+        our website, users can browse through a variety of schools in their area
+        and choose the one that best suits their needs. Once a school has been
+        selected, our platform makes it effortless to acquire a place. With just
+        a few clicks, users can submit their application and complete the
+        acquiring place hassle-free. Our team is dedicated to helping students
+        achieve their academic goals. Whether you are a parent seeking the
+        perfect school for your child or a student ready to take the next step
+        in your educational journey, we are here to help. We offer valuable
+        resources, tips, and guidance to support students every step of the way.
+        Thank you for choosing our website, and we look forward to being a part
+        of your academic journey.
+      </p>
+      <hr />
+      <br />
+      <br />
+      <p>
+        እንኳን ወደ ድህረ ገጻችን በደህና መጡ! በዚ ድህረ ገጽ ተማሪዎች እና ወላጆች በኢትዮጵያ ውስጥ ላሉ ትምህርት
+        ቤቶች ቦታ ለማስያዝ እንዲችሉ ለመርዳት ታስቦ የተዘጋጀ ነው። የትምህርት ቤት ምርጫ እና ምዝገባ ሂደት ምን ያህል
+        ከባድ እና ጊዜ የሚወስድ እንደሆነ እንረዳለን፣ለዚህም ነው አጠቃላይ ሂደቱን ለማቃለል ቀላል እና ምቹ ለማረግ ይህን
+        የፈጠርነው። በድረ-ገጻችን በአካባቢዮት ያሉ የተለያዩ ትምህርት ቤቶችን ማሰስ እና ለፍላጎቶ የሚስማማውን መምረጥ
+        ይችላሉ። ከመርጡ በሁአላም፣ ተጠቃሚዎች ማመልከቻቸውን ማስገባት እና ቦታ ማስያዝ ይችላሉ። ቀጥሎም የመርጣችሁት
+        ትምህርት ቤት ከጥታ እናንተን ያነጋግርዎታል::
+      </p>
+      <p>የእኛን ድረ-ገጽ ስለመረጡ እናመሰግናለን!</p>
+    </div>
+  );
+};
+
+export default About;
