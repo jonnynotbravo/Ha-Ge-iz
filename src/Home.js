9a5c5049eff75962d40a4fa8fc14bbf746f7caf9
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-05-07 14:59:10 -0400

    Steps draft started

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,13 @@
 import Header from "./Header";
 import SlideShow from "./SlideShow";
+import Steps from "./Steps";
+
 const Home = () => {
   return (
     <div>
       <Header />
       <SlideShow />
+      <Steps />
     </div>
   );
 };
---
+++ src/Steps.js
@@ -0,0 +1,48 @@
+const Steps = () => {
+  return (
+    <div id='stepsOuter'>
+      <h1>Steps</h1>
+      <div id="steps">
+        <section>
+          <div className="container">
+            <div className="card">
+              <div className="content">
+                <div className="contentBx">
+                  <h3>
+                    01
+                    <br />
+                    <span>Browse the schools listed on our page.</span>
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="content">
+                <div className="contentBx">
+                  <h3>
+                    02
+                    <br />
+                    <span>Find a school that best fits your needs.</span>
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="content">
+                <div className="contentBx">
+                  <h3>
+                    03
+                    <br />
+                    <span>Fill out the form and acquire a place.</span>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Steps;
