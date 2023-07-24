866b097a6397aaeaaf2bd95027a965cf177fa4ea
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-07-23 23:50:46 -0400

    Created .env and configured it

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1kdzw1qvftb2CX48r6chYSHvF6vBHZXA",
-  authDomain: "school-registration-79a19.firebaseapp.com",
-  projectId: "school-registration-79a19",
-  storageBucket: "school-registration-79a19.appspot.com",
-  messagingSenderId: "434335025259",
-  appId: "1:434335025259:web:f6f10a193fe884ed79892e",
-  measurementId: "G-B19QHPVQ0K",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
