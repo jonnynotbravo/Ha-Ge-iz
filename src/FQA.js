ba5cd6c37f6956f11e4c9b30ac41e53d27ad423a
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-07-26 22:59:12 -0400

    FQA draft completed

--- src/FQA.js
+++ src/FQA.js
@@ -1,28 +1,48 @@
 import React, { useState } from "react";
 
 function FAQ() {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(-1);
 
   const handleAccordionClick = (index) => {
-    setActiveIndex(index === activeIndex ? -1 : index);
+    setActiveIndex((prevIndex) => (prevIndex === index ? -1 : index));
   };
 
   const faqItems = [
     {
-      id: 1,
+      id: 3,
       question: "What is this website used for?",
       answer:
-        "blah blah blah blah blahblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+        "This website is used for booking reservations at various establishments such as hotels, restaurants, and event venues. It provides a user-friendly interface for users to find and book their desired reservations.",
     },
     {
       id: 2,
-      question: "is the reservation guaranteed?",
-      answer: "NO!!!",
+      question: "Is the reservation guaranteed?",
+      answer: "Yes, all reservations made through this website are guaranteed.",
     },
     {
-      id: 3,
-      question: "Who made this ?",
-      answer: "Me",
+      id: 1,
+      question: "Who made this website?",
+      answer:
+        "This website was developed by a team of skilled web developers from XYZ Company.",
+    },
+    {
+      id: 4,
+      question: "How do I make a reservation on the website?",
+      answer:
+        "To make a reservation, navigate to the 'Reservations' section and select the type of establishment you want to book (e.g., hotel, restaurant, event venue). Then, choose the date, time, and number of guests, and click on the 'Book Now' button. Follow the prompts to complete the reservation.",
+    },
+    {
+      id: 5,
+      question: "Are there any fees associated with reservations?",
+      answer:
+        "No, we are transparent about our fees. The price you see during the reservation process includes all applicable taxes and fees. There are no hidden charges.",
+    },
+    {
+      id: 6,
+      question:
+        "How long will it take to hear back after making a reservation?",
+      answer:
+        "After completing the reservation process, you will receive a reservation confirmation via email. Within 5 - 10 business days, you can expect a call from the establishment you reserved at to discuss the next steps.",
     },
   ];
 
@@ -38,6 +58,8 @@ function FAQ() {
             onClick={() => handleAccordionClick(index)}
           >
             {item.question}
+            <span className="hint"></span>
+            <span className="arrow">&#9662;</span>
           </div>
           <div
             className={`accordion-content ${
