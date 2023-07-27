ac249776175dee2558a7ad11bbe3828405752ddf
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-07-26 22:24:34 -0400

    Form Draft Done!

--- src/Form.js
+++ src/Form.js
@@ -1,98 +1,168 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Form = () => {
   const { id } = useParams();
+  const [gradeToBe, setGradeToBe] = useState("");
+  const [showWarning, setShowWarning] = useState(false);
+
+  const handleGradeChange = (event) => {
+    const grade = parseInt(event.target.value, 10);
+    if (grade >= 1 && grade <= 12) {
+      setGradeToBe(grade);
+      setShowWarning(false);
+    } else {
+      setGradeToBe("");
+      setShowWarning(true);
+    }
+  };
+
+  const handleSubmit = () => {
+    // Add your form submission logic here
+    console.log("Form submitted!");
+  };
 
   return (
     <div style={formContainerStyles}>
-      <h1 style={formTitleStyles}>Form</h1>
-      <div style={formFieldStyles}>
-        <label style={labelStyles}>First Name:</label>
-        <input
-          type="text"
-          placeholder="Enter First Name"
-          style={inputStyles}
-        />
-      </div>
-      <div style={formFieldStyles}>
-        <label style={labelStyles}>Last Name:</label>
-        <input
-          type="text"
-          placeholder="Enter Last Name"
-          style={inputStyles}
-        />
-      </div>
-      <div style={formFieldStyles}>
-        <label style={labelStyles}>Birth Date:</label>
-        <input
-          type="date"
-          placeholder="Enter Your Age"
-          style={inputStyles}
-        />
-      </div>
-      <div style={formFieldStyles}>
-        <label style={labelStyles}>Gender:</label>
-        <input
-          type="text"
-          placeholder="Enter Your Gender"
-          style={inputStyles}
-        />
-      </div>
-      <div style={formFieldStyles}>
-        <label style={labelStyles}>Grade:</label>
-        <input
-          type="number"
-          min="1"
-          max="12"
-          placeholder="Enter Your Grade"
-          style={inputStyles}
-        />
-      </div>
-      <div style={formFieldStyles}>
-        <label style={labelStyles}>School ID:</label>
-        <input
-          type="text"
-          defaultValue={id}
-          readOnly
-          style={{ ...inputStyles, ...readOnlyInputStyles }}
-        />
+      <div style={outerLayerStyles}>
+        <h1 style={formTitleStyles}>Form</h1>
+        <div style={formFieldStyles}>
+          <label style={labelStyles}>First Name:</label>
+          <input
+            type="text"
+            placeholder="Enter First Name"
+            style={inputStyles}
+          />
+        </div>
+        <div style={formFieldStyles}>
+          <label style={labelStyles}>Last Name:</label>
+          <input
+            type="text"
+            placeholder="Enter Last Name"
+            style={inputStyles}
+          />
+        </div>
+        <div style={formFieldStyles}>
+          <label style={labelStyles}>Birth Date:</label>
+          <input type="date" placeholder="Enter Your Age" style={inputStyles} />
+        </div>
+        <div style={formFieldStyles}>
+          <label style={labelStyles}>Gender:</label>
+          <select style={inputStyles}>
+            <option value="" disabled selected>
+              Select Gender
+            </option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+        </div>
+        <div style={formFieldStyles}>
+          <label style={labelStyles}>Grade To be:</label>
+          <input
+            type="number"
+            min="1"
+            max="12"
+            placeholder="Enter Your Grade To be"
+            style={{
+              ...inputStyles,
+              ...gradeToBeInputStyles,
+              borderColor: showWarning ? "red" : "#ccc",
+            }}
+            value={gradeToBe}
+            onChange={handleGradeChange}
+          />
+          {showWarning && (
+            <span style={warningStyles}>Grade should be between 1 and 12.</span>
+          )}
+        </div>
+        <div style={formFieldStyles}>
+          <label style={labelStyles}>School:</label>
+          <input
+            type="text"
+            defaultValue={id}
+            readOnly
+            style={{ ...inputStyles, ...readOnlyInputStyles }}
+          />
+        </div>
+        <div style={submitButtonStyles}>
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
       </div>
     </div>
   );
 };
 
+// CSS styles (same as before)
+// ...
+
+const submitButtonStyles = {
+  textAlign: "center",
+  marginTop: "20px",
+};
+
+const gradeToBeInputStyles = {
+  padding: "15px",
+  border: "2px solid #ccc",
+  borderRadius: "5px",
+  width: "300px",
+  fontSize: "18px",
+  fontFamily: "Helvetica, Arial, sans-serif",
+};
+
+const warningStyles = {
+  display: "block",
+  color: "red",
+  fontSize: "14px",
+  fontFamily: "Helvetica, Arial, sans-serif",
+  marginTop: "5px",
+};
+
+// CSS styles (same as before)
+// ...
+
 // CSS styles
 const formContainerStyles = {
   display: "flex",
-  flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
-  padding: "20px",
+  minHeight: "100vh",
+  background: "#f0f0f0",
+};
+
+const outerLayerStyles = {
+  padding: "30px",
+  background: "#ffffff",
+  borderRadius: "10px",
+  boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
 };
 
 const formTitleStyles = {
-  fontSize: "24px",
+  fontSize: "36px",
   fontWeight: "bold",
   marginBottom: "20px",
+  textAlign: "center",
+  fontFamily: "Helvetica, Arial, sans-serif",
 };
 
 const formFieldStyles = {
   display: "flex",
   alignItems: "center",
-  marginBottom: "15px",
+  marginBottom: "20px",
 };
 
 const labelStyles = {
   width: "150px",
-  fontSize: "16px",
+  fontSize: "24px",
+  fontFamily: "Helvetica, Arial, sans-serif",
 };
 
 const inputStyles = {
-  padding: "8px",
+  padding: "15px",
   border: "1px solid #ccc",
   borderRadius: "5px",
-  width: "250px",
+  width: "300px",
+  fontSize: "18px",
+  fontFamily: "Helvetica, Arial, sans-serif",
 };
 
 const readOnlyInputStyles = {
