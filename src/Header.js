3ce0074d92b7547a57a261c79e85a8354be63dd8
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-08-05 12:53:30 -0400

    Header click event updated

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,13 @@
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const handleClick = () => {
+    window.scrollTo(0, 0); // Scroll to the top of the page
+  };
   return (
     <header id="header">
       <nav className="links" style={{ "--items": 5 }}>
-        <NavLink to="/" id="home">
+        <NavLink to="/" id="home" onClick={handleClick}>
           Home
         </NavLink>
         <NavLink to="/about">About</NavLink>
